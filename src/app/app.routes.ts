111069cda560b680aa61c3928b9d6ffd4c42db20
pduracell513-collab <pduracell513@gmail.com>
2025-11-27 02:05:36 -0600

    creacion de pages convocatoria y monitoreo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Padron } from './pages/padron/padron';
+import { Convocatorias } from './pages/convocatorias/convocatorias';
+import { Monitoreo } from './pages/monitoreo/monitoreo';
 export const routes: Routes = [
     {path: '', component: Dashboard},
     {path: 'padron', component: Padron},
+    {path: 'convocatorias', component: Convocatorias},
+    {path: 'monitoreo', component: Monitoreo},
     {path: '**', redirectTo: '' }
 ];
 
